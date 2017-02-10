11f44340a5b10e3ceaf50c7d7509bcfb077715a6
Jared M. Smith <absynce@gmail.com>
2017-02-10 17:05:00 -0600

    Add additional example of immutability compile checking

--- src/01-immutable-record.elm
+++ src/01-immutable-record.elm
@@ -4,10 +4,13 @@ darkSide = { artist = "Pinky Floyd", released = 1973 }
 -- try to update it
 darkSide.artist = "Syd Barret" -- will fail to compile
 
--- this will fail too, types don't match
-darkSideWithTitle = { darkSide | title = "Dark Side of the Moon" }
+-- this fails because it's trying to modify an existing record (darkSide)
+darkSide = { darkSide | artist = "Pink Floyd" }
 
 -- create a new record, combining properties from another, updating one
 -- Pipe (|) is reminiscent of OR in bit logic.
 -- update artist
 darkSideCorrectArtist = { darkSide | artist = "Pink Floyd" }
+
+-- this will fail too, types don't match
+darkSideWithTitle = { darkSide | title = "Dark Side of the Moon" }
