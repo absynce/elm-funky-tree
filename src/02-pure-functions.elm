1916406fa5b3fe32a7529923c4db3ffd454888e4
Jared M. Smith <absynce@gmail.com>
2017-02-14 10:25:14 -0600

    Add functional purity example
    Next step: add higher-order function examples.

---
+++ src/02-pure-functions.elm
@@ -0,0 +1,35 @@
+module Main exposing (..)
+
+import Char
+import Html exposing (text, div)
+import String exposing (..)
+
+
+main =
+    div []
+        [ jared |> toString |> text
+        , jared |> capitalizeName |> toString |> text
+          -- , jared |> toString |> text -- prove not mutated
+        ]
+
+
+jared =
+    { name = "jared", age = 32 }
+
+
+{-| Cannot update existing record the same as we learned in 01-immutable-record
+-}
+capitalizeName who =
+    { who | name = capitalize who.name }
+
+
+{-| Source: https://groups.google.com/d/msg/elm-discuss/Z6M8MJIWNsk/8jlJatjcCTkJ
+-}
+capitalize : String -> String
+capitalize string =
+    case uncons string of
+        Nothing ->
+            ""
+
+        Just ( head, tail ) ->
+            cons (Char.toUpper head) tail
